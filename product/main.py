3999f3b21437b8a7b512f225d38f09a30003e5a4
avnms <sarmaakondivnm@icloud.com>
2024-03-19 17:39:52 +1100

    Post data to database

--- product/main.py
+++ product/main.py
@@ -1,13 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 from .schemas import Product
 from . import models
-from .database import engine
+from .database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.post("/product")
-def add_product(request: Product):
+def add_product(request: Product, db: Session = Depends(get_db)):
+    new_product = models.Product(
+        name=request.name,
+        description=request.description,
+        price=request.price,
+    )
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
     return request
